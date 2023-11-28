cc181a51f607cbbadac40960089a9535451c0b38
Byron Dowling <byrondowling7@gmail.com>
2023-11-28 12:03:22 -0500

    Create xentSecondAtempt.py

---
+++ Parts 3, 4, and 5/xentSecondAtempt.py
@@ -0,0 +1,263 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import models
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision.utils import make_grid
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 16, 5)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.dropout = nn.Dropout(0.2)
+        self.fc1 = nn.Linear(32*53*53, 256)
+        self.fc2 = nn.Linear(256, 84)
+        self.fc3 = nn.Linear(84, 2)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = x.view(-1, 32 * 53 * 53)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.softmax(self.fc3(x))
+        return x
+
+
+if __name__ == '__main__':
+    train_dataset_path = r'C:\Users\byron\OneDrive\Documents\Python Stuff\Computer Vision\Driving_Project\Training'
+    test_dataset_path = r'C:\Users\byron\OneDrive\Documents\Python Stuff\Computer Vision\Driving_Project\Validation'
+
+    ###################################################################################################
+    """
+        1280 x 1024 original size
+        640 x 512
+        325 x 256
+    """
+    batch_size = 32
+
+    training_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(20),
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+    ])
+
+    data = datasets.ImageFolder(r'C:\Users\byron\OneDrive\Documents\Python Stuff\Computer Vision\Driving_Project',transform=training_transforms)
+
+    ###################################################################################################
+
+    # percentage of training set to use as validation
+    test_size = 0.3
+    valid_size = 0.1
+
+    classes = ('Dangerous', 'Not Dangerous')
+
+    #For test
+    num_data = len(data)
+    indices_data = list(range(num_data))
+    np.random.shuffle(indices_data)
+    split_tt = int(np.floor(test_size * num_data))
+    train_idx, test_idx = indices_data[split_tt:], indices_data[:split_tt]
+
+    #For Valid
+    num_train = len(train_idx)
+    indices_train = list(range(num_train))
+    np.random.shuffle(indices_train)
+    split_tv = int(np.floor(valid_size * num_train))
+    train_new_idx, valid_idx = indices_train[split_tv:],indices_train[:split_tv]
+
+
+    # define samplers for obtaining training and validation batches
+    train_sampler = SubsetRandomSampler(train_new_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    ###################################################################################################
+
+    train_loader = torch.utils.data.DataLoader(data, batch_size=batch_size,
+        sampler=train_sampler, num_workers=1)
+
+    valid_loader = torch.utils.data.DataLoader(data, batch_size=batch_size, 
+        sampler=valid_sampler, num_workers=1)
+
+    test_loader = torch.utils.data.DataLoader(data, sampler = test_sampler, batch_size=batch_size, 
+        num_workers=1)
+
+    classes = [0,1]
+
+    len(test_loader)*batch_size + len(valid_loader)*batch_size + len(train_loader)*batch_size
+
+    ###################################################################################################
+
+    # helper function to un-normalize and display an image
+    def imshow(img):
+        img = img / 2 + 0.5  # unnormalize
+        plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
+        
+    # obtain one batch of training images
+    dataiter = iter(train_loader)
+
+    images, labels = next(dataiter)
+
+    # show images
+    imshow(make_grid(images))
+
+    # print labels
+    # print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    ###################################################################################################
+
+    train_on_gpu = torch.cuda.is_available()
+
+
+    model = Net()
+    print(model)
+
+    # move tensors to GPU if CUDA is available
+    if train_on_gpu:
+        print('Cuda is available!')
+        model.cuda()
+
+
+    # specify loss function
+    criterion = torch.nn.CrossEntropyLoss()
+
+    # specify optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr = 0.003, momentum= 0.9)
+
+
+    ###################################################################################################
+
+    # number of epochs to train the model
+    n_epochs = 50
+
+    valid_loss_min = np.Inf # track change in validation loss
+
+    for epoch in range(1, n_epochs+1):
+
+        # keep track of training and validation loss
+        train_loss = 0.0
+        valid_loss = 0.0
+        
+        ###################
+        # train the model #
+        ###################
+        model.train()
+        for data, target in train_loader:
+            # move tensors to GPU if CUDA is available
+            if train_on_gpu:
+                data, target = data.cuda(), target.cuda()
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = model(data)
+            # calculate the batch loss
+            loss = criterion(output, target)
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            # perform a single optimization step (parameter update)
+            optimizer.step()
+            # update training loss
+            train_loss += loss.item()*data.size(0)
+            
+        ######################    
+        # validate the model #
+        ######################
+        model.eval()
+        for data, target in valid_loader:
+            # move tensors to GPU if CUDA is available
+            if train_on_gpu:
+                data, target = data.cuda(), target.cuda()
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = model(data)
+            # calculate the batch loss
+            loss = criterion(output, target)
+            # update average validation loss 
+            valid_loss += loss.item()*data.size(0)
+        
+        # calculate average losses
+        train_loss = train_loss/len(train_loader.dataset)
+        valid_loss = valid_loss/len(valid_loader.dataset)
+            
+        # print training/validation statistics 
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, train_loss, valid_loss))
+        
+        # save model if validation loss has decreased
+        if valid_loss <= valid_loss_min:
+            print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+            valid_loss_min,
+            valid_loss))
+            torch.save(model.state_dict(), 'model_cifar.pt')
+            valid_loss_min = valid_loss
+
+    ###################################################################################################
+
+    # track test loss
+    test_loss = 0.0
+    class_correct = list(0. for i in range(2))
+    class_total = list(0. for i in range(2))
+
+    model.eval()
+    i=1
+    # iterate over test data
+    len(test_loader)
+    for data, target in test_loader:
+        i=i+1
+        if len(target)!=batch_size:
+            continue
+            
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # update test loss 
+        test_loss += loss.item()*data.size(0)
+        # convert output probabilities to predicted class
+        _, pred = torch.max(output, 1)    
+        # compare predictions to true label
+        correct_tensor = pred.eq(target.data.view_as(pred))
+        correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+        # calculate test accuracy for each object class
+    #     print(target)
+        
+        for i in range(batch_size):       
+            label = target.data[i]
+            class_correct[label] += correct[i].item()
+            class_total[label] += 1
+
+    # average test loss
+    test_loss = test_loss/len(test_loader.dataset)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    for i in range(2):
+        if class_total[i] > 0:
+            print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+                classes[i], 100 * class_correct[i] / class_total[i],
+                np.sum(class_correct[i]), np.sum(class_total[i])))
+        else:
+            print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+    print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+        100. * np.sum(class_correct) / np.sum(class_total),
+        np.sum(class_correct), np.sum(class_total)))
